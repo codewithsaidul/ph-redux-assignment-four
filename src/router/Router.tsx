fa1ce0c38598a62c40c69b02ee4a2c5b9c5e2409
codewithsaidul <codewithsaidul@gmail.com>
2025-07-01 19:06:59 +0600

    assignment-4 initialized

---
+++ src/router/Router.tsx
@@ -0,0 +1,21 @@
+import MainLayout from "@/Layout/MainLayout";
+import AllBooks from "@/pages/Books/AllBooks";
+import Home from "@/pages/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/books",
+        Component: AllBooks,
+      },
+    ],
+  },
+]);
